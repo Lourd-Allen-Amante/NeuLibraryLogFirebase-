3b5c91f2ff57d58e4b06b16b8eb65c9199e1ef42
Lourd Allen G. Amante <lourdallen.amante@neu.edu.ph>
2026-03-16 10:58:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5463545232-3ebe6",
+  "appId": "1:360519847040:web:d69dc9c99efac9e9caea65",
+  "apiKey": "AIzaSyDq5JyhqQ64GR647wHjeXVuUjvdJWrcfkI",
+  "authDomain": "studio-5463545232-3ebe6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "360519847040"
+};
