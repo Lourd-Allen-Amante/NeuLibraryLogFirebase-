c9b17eae1f552f35eb44c26744005cd430afd7a3
Lourd Allen Amante <lourdallen30@gmail.com>
2026-03-23 12:27:54 +0800

    Update Firebase config to use environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
-import { FirebaseOptions } from 'firebase/app';
-
 export const firebaseConfig: FirebaseOptions = {
-  projectId: "studio-5463545232-3ebe6",
-  appId: "1:360519847040:web:d69dc9c99efac9e9caea65",
-  apiKey: "AIzaSyDq5JyhqQ64GR647wHjeXVuUjvdJWrcfkI",
-  authDomain: "studio-5463545232-3ebe6.firebaseapp.com",
-  storageBucket: "studio-5463545232-3ebe6.appspot.com",
-  messagingSenderId: "360519847040",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
